35b24ee2c69ec28517513bacb224fcb0df24d599
George Cushen <gcushen@users.noreply.github.com>
2015-05-12 01:36:08 +0100

    1. Added router funtionality to compliment newly added ViewSets in views.py. 2. Changed API docs URL to separate from browserable web API index. 3. Added auth and token URLs. 4. PEP8 tidy up

--- mezzanine_api/urls.py
+++ mezzanine_api/urls.py
@@ -1,34 +1,19 @@
 from __future__ import unicode_literals
-from django.conf.urls import patterns, url, include
-from .views import UserList, UserDetail
-from .views import CategoryList, CategoryDetail
-from .views import PageList, PageDetail
-from .views import PostList, PostDetail
+from django.conf.urls import url, include
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework import routers
+from .views import UserViewSet, PostViewSet, CategoryViewSet, PageViewSet
 
-user_urls = patterns('',
-    url(r'^/(?P<id>[0-9]+)$', UserDetail.as_view(), name='user-detail'),
-    url(r'^$', UserList.as_view(), name='user-list')
-)
 
-category_urls = patterns('',
-    url(r'^/(?P<id>[0-9]+)$', CategoryDetail.as_view(), name='category-detail'),
-    url(r'^$', CategoryList.as_view(), name='category-list')
-)
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'pages', PageViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'categories', CategoryViewSet)
 
-page_urls = patterns('',
-    url(r'^/(?P<id>[0-9]+)$', PageDetail.as_view(), name='page-detail'),
-    url(r'^$', PageList.as_view(), name='page-list')
-)
-
-post_urls = patterns('',
-    url(r'^/(?P<id>\d+)$', PostDetail.as_view(), name='post-detail'),
-    url(r'^$', PostList.as_view(), name='post-list')
-)
-
-urlpatterns = patterns('',
-    url(r'^users', include(user_urls)),
-    url(r'^categories', include(category_urls)),
-    url(r'^pages', include(page_urls)),
-    url(r'^posts', include(post_urls)),
-    url("^", include('rest_framework_swagger.urls')),
-)
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^token/', obtain_auth_token, name='api-token'),
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
