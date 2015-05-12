a1f23c8e848d32d1d8cf44ee34c92adad68dbaa9
George Cushen <gcushen@users.noreply.github.com>
2015-05-12 01:30:29 +0100

    1. Replaced APIViews with ViewSets in order to combine the logic for related views into a single class. 2. Implemented pagination for improved handling of large datasets. 3. Allow blog posts to be filtered by category. 4. Sorted blog post list by publish_date descending. 5. PEP8 tidy up

--- mezzanine_api/views.py
+++ mezzanine_api/views.py
@@ -1,76 +1,55 @@
-from rest_framework import generics, permissions
+from rest_framework import viewsets, filters, permissions
+from rest_framework.pagination import PageNumberPagination
 from .serializers import UserSerializer, PostSerializer, CategorySerializer, PageSerializer
-from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
 from mezzanine.blog.models import BlogPost as Post, BlogCategory
 from mezzanine.pages.models import Page
-from mezzanine.generic.models import ThreadedComment
+#from django.contrib.sites.models import Site
+#from mezzanine.generic.models import ThreadedComment
 
 
-class UserList(generics.ListAPIView):
+class CustomPagination(PageNumberPagination):
     """
-    API endpoint that allows all users to be viewed
+    Let large result sets be split into individual pages of data
     """
-    
+    page_size = 25
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    For listing or retrieving users.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    pagination_class = CustomPagination
 
-class UserDetail(generics.RetrieveAPIView):
-    """
-    API endpoint that allows a specific user to be viewed
-    """
-    
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    lookup_field = 'id'
 
-class CategoryList(generics.ListAPIView):
+class PageViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows all categories to be viewed
+    For listing or retrieving pages.
     """
-    
-    queryset = BlogCategory.objects.all()
-    serializer_class = CategorySerializer
-
-class CategoryDetail(generics.RetrieveAPIView):
-    """
-    API endpoint that allows a specific category to be viewed
-    """
-    
-    queryset = BlogCategory.objects.all()
-    serializer_class = CategorySerializer
-    lookup_field = 'id'
-
-class PageList(generics.ListAPIView):
-    """
-    API endpoint that allows all pages to be viewed
-    """
-    
     queryset = Page.objects.all()
     serializer_class = PageSerializer
+    pagination_class = CustomPagination
 
-class PageDetail(generics.RetrieveAPIView):
-    """
-    API endpoint that allows a specific page to be viewed
-    """
-    
-    queryset = Page.objects.all()
-    serializer_class = PageSerializer
-    lookup_field = 'id'
 
-class PostList(generics.ListAPIView):
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows all blog posts to be viewed
+    For listing or retrieving blog categories.
     """
-    
-    queryset = Post.objects.all()
+    queryset = BlogCategory.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = CustomPagination
+
+
+class PostViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    For listing or retrieving blog posts.
+    """
+    queryset = Post.objects.all().order_by("-publish_date")
+    filter_fields = ('categories',)
+    filter_backends = (filters.DjangoFilterBackend,)
     serializer_class = PostSerializer
-
-class PostDetail(generics.RetrieveAPIView):
-    """
-    API endpoint that allows a specific blog post to be viewed
-    """
-    
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    lookup_field = 'id'
+    pagination_class = CustomPagination
