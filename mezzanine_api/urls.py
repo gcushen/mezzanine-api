8ec47bed2db6b42f21958dedf7da07f4843c3f15
George Cushen <gcushen@users.noreply.github.com>
2015-05-13 01:36:06 +0100

    1. Removed trailing slashes from endpoints. 2. Added Oauth2 support. 3. Removed DRF auth token support.

--- mezzanine_api/urls.py
+++ mezzanine_api/urls.py
@@ -1,11 +1,10 @@
 from __future__ import unicode_literals
 from django.conf.urls import url, include
-from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework import routers
 from .views import UserViewSet, PostViewSet, CategoryViewSet, PageViewSet
 
 
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'users', UserViewSet)
 router.register(r'pages', PageViewSet)
 router.register(r'posts', PostViewSet)
@@ -14,6 +13,6 @@ router.register(r'categories', CategoryViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^docs/', include('rest_framework_swagger.urls')),
-    url(r'^token/', obtain_auth_token, name='api-token'),
+    url(r'^oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
