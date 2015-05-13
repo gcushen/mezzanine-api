dafac914b599d77eb2e06510525cb9a6060abb7c
George Cushen <gcushen@users.noreply.github.com>
2015-05-13 18:10:32 +0100

    1. Added DEFAULT_AUTHENTICATION_CLASSES. 2. Added OAUTH2_PROVIDER. 3. Updated SWAGGER_SETTINGS.info.

--- mezzanine_api/settings.py
+++ mezzanine_api/settings.py
@@ -1,7 +1,17 @@
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+    ),
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
+
+OAUTH2_PROVIDER = {
+    # this is the list of available scopes
+    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
 }
 
 SWAGGER_SETTINGS = {
@@ -13,8 +23,11 @@ SWAGGER_SETTINGS = {
     'is_superuser': False,
     'permission_denied_handler': None,
     'info': {
-        'title': 'Web API Documentation',
-        'description': 'The web API package exposes Mezzanine data through a REST API using JSON serialization. Bugs should be reported on <a href="https://github.com/gcushen/mezzanine-api/issues" target="_blank">Github</a>.',
+        'title': 'REST API Resource Documentation',
+        'description': 'The RESTful web API exposes Mezzanine data using JSON serialization. This interactive document '
+                       'will guide you through the relevant data structures, API endpoints, filtering, and searching. '
+                       'Bugs should be reported on'
+                       ' <a href="https://github.com/gcushen/mezzanine-api/issues" target="_blank">Github</a>.',
     },
     'doc_expansion': 'none',
 }
