3c7107350e7a20080022709fc0e29c4d29cd30f2
George Cushen <gcushen@users.noreply.github.com>
2016-03-01 19:24:32 +0000

    Tidy up

--- tests/settings.py
+++ tests/settings.py
@@ -162,27 +162,27 @@ OPTIONAL_APPS = (
     PACKAGE_NAME_GRAPPELLI,
 )
 
-#####################
-# REST API SETTINGS #
-#####################
-try:
-    from mezzanine_api.settings import *  # NOQA
-except ImportError:
-    pass
+##################
+# LOCAL SETTINGS #
+##################
+# Included above
+
 
 ####################
 # DYNAMIC SETTINGS #
 ####################
-
-# set_dynamic_settings() will rewrite globals based on what has been
-# defined so far, in order to provide some better defaults where
-# applicable. We also allow this settings module to be imported
-# without Mezzanine installed, as the case may be when using the
-# fabfile, where setting the dynamic settings below isn't strictly
-# required.
 try:
     from mezzanine.utils.conf import set_dynamic_settings
 except ImportError:
     pass
 else:
     set_dynamic_settings(globals())
+
+
+#####################
+# REST API SETTINGS #
+#####################
+try:
+    from mezzanine_api.settings import *  # NOQA
+except ImportError as e:
+    pass
