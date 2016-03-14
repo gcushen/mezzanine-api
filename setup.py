f6cefc014a3c033d66e617bf03040aa5f43a0f2a
George Cushen <gcushen@users.noreply.github.com>
2016-03-14 23:38:17 +0000

    Fix setup: was excluding HTML files

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import os
 import sys
 import re
+import shutil
 from setuptools import setup, find_packages
 
 
@@ -9,6 +10,9 @@ def get_version(package):
     """Return package version as listed in `__version__` in `init.py`."""
     init_py = open(os.path.join(package, '__init__.py')).read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('mezzanine_api')
 
 
 if sys.argv[-1] == 'publish':
@@ -21,29 +25,32 @@ if sys.argv[-1] == 'publish':
     os.system("python setup.py sdist bdist_wheel")
     os.system("twine upload dist/*")
     print("You probably want to also tag the version now:")
-    print("  git tag -a {0} -m 'version {0}'".format(get_version("mezzanine_api")))
+    print("  git tag -a {0} -m 'version {0}'".format(version))
     print("  git push --tags")
+    shutil.rmtree('dist')
+    shutil.rmtree('build')
+    shutil.rmtree('mezzanine_api.egg-info')
     sys.exit()
 
 
 setup(
     name='mezzanine-api',
-    version=get_version("mezzanine_api"),
+    version=version,
     author='George Cushen',
     author_email='mezzanine-users@googlegroups.com',   
     url='http://gcushen.github.io/mezzanine-api',
     description='A RESTful web API for Mezzanine CMS.',
     long_description=open("README.rst", 'rb').read().decode('utf-8'),
-    keywords='mezzanine rest restful web api',
+    keywords='mezzanine cms api rest restful web',
     include_package_data=True,
     license='BSD',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests', 'site']),
     install_requires=[
-        "Mezzanine >= 4.1.0",
-        "django-rest-swagger==0.3.5",
-        "djangorestframework==3.3.2",
+        "Mezzanine>=4.1.0",
+        "django-rest-swagger>=0.3.5, <1.0.0",
+        "djangorestframework>=3.3.2, <4.0.0",
         "django-filter",
-        "django-oauth-toolkit==0.10.0"
+        "django-oauth-toolkit>=0.10.0, <1.0.0"
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
