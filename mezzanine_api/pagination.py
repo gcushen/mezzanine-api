7e01b149855a96383f3c542ff4b77d751dd662b8
George Cushen <gcushen@users.noreply.github.com>
2016-04-13 19:04:04 +0100

    Switch from Page# to Limit Offset Pagination

--- mezzanine_api/pagination.py
+++ mezzanine_api/pagination.py
@@ -1,19 +1,18 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 from mezzanine.conf import settings
 
 
-class MezzaninePagination(PageNumberPagination):
+class MezzaninePagination(LimitOffsetPagination):
     """
     Default pagination class.
     Let large result sets be split into individual pages of data.
     """
-    page_size = 20
-    page_size_query_param = 'per_page'
-    max_page_size = getattr(settings, 'MZN_API_MAX_PER_PAGE', 100)
+    default_limit = 20
+    max_limit = getattr(settings, 'MZN_API_LIMIT_MAX', 100)
 
 
 class PostPagination(MezzaninePagination):
     """
     Pagination for Blog Posts
     """
-    page_size = 10
+    default_limit = 10
