8dfefd74d40d1a16ea8880e04ff9992ff91803dd
George Cushen <gcushen@users.noreply.github.com>
2016-04-13 15:27:21 +0100

    Add Material theme and refactor

--- mezzanine_api/templatetags/mezzanine_api.py
+++ mezzanine_api/templatetags/mezzanine_api.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from mezzanine import template
+from mezzanine import conf
 from .. import __version__
 
 register = template.Library()
@@ -8,3 +9,8 @@ register = template.Library()
 @register.simple_tag
 def get_mezzanine_api_version():
     return __version__
+
+
+@register.simple_tag
+def get_mezzanine_api_doc_title():
+    return getattr(conf.settings, 'MZN_API_DOC_TITLE', '')
