17f12fda11017f0a4806c00428bd51518f6e3c75
thijs <thijs@go2people.nl>
2018-07-10 13:27:41 +0200

    Middleware format upated to django 1.11

--- mezzanine_api/middleware.py
+++ mezzanine_api/middleware.py
@@ -3,43 +3,41 @@ from django import http
 from rest_framework.reverse import reverse
 from mezzanine.conf import settings
 
-# Add middleware compatibility for django 1.11 and up.
-try:
-    from django.utils.deprecation import MiddlewareMixin
-except ImportError:
-    MiddlewareMixin = object
 
+class ApiMiddleware:
+    """Mezzanine API Middleware"""
 
-class ApiMiddleware(MiddlewareMixin):
-    """
-    Mezzanine API Middleware
-    """
+    def __init__(self, get_response):
+        """One-time configuration and initialization."""
+        self.get_response = get_response
 
-    def process_request(self, request):
-        """
-        Process OPTIONS request
-        """
-        if request.method == 'OPTIONS' and 'HTTP_ACCESS_CONTROL_REQUEST_METHOD' in request.META:
+    def __call__(self, request):
+        """Code to be executed by middleware"""
+        # Process OPTIONS request
+        if request.method == 'OPTIONS' \
+                and 'HTTP_ACCESS_CONTROL_REQUEST_METHOD' in request.META:
             response = http.HttpResponse()
             return response
-        return None
 
-    def process_response(self, request, response):
-        """
-        Add the CORS headers and API discovery header for clients
-        """
-        response['X-Api-Discovery'] = request.build_absolute_uri(reverse('api-root'))
+        response = self.get_response(request)
 
+        # Add API discovery header for clients
+        response['X-Api-Discovery'] = \
+            request.build_absolute_uri(reverse('api-root'))
+
+        # Add the CORS headers
         if self.is_api_request(request):
-
             if settings.MZN_API_CORS_ORIGIN_ALLOW_ALL:
                 response['Access-Control-Allow-Origin'] = '*'
 
             if request.method == 'OPTIONS':
-                response['Access-Control-Allow-Headers'] = ', '.join(settings.MZN_API_CORS_ALLOW_HEADERS)
-                response['Access-Control-Allow-Methods'] = ', '.join(settings.MZN_API_CORS_ALLOW_METHODS)
+                response['Access-Control-Allow-Headers'] = \
+                    ', '.join(settings.MZN_API_CORS_ALLOW_HEADERS)
+                response['Access-Control-Allow-Methods'] = \
+                    ', '.join(settings.MZN_API_CORS_ALLOW_METHODS)
 
         return response
 
     def is_api_request(self, request):
+        """Returns true if request.path contains the api root URL"""
         return re.match(r'^' + reverse('api-root') + '.*$', request.path)
